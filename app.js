006881612323120fc2bdda0cf45000516bae3a9b
antonykmak <antony3357@hotmail.com>
2025-01-19 15:28:27 -0500

    Actualiza app.js

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById('amigo').value;
+    if (input != '') {
+        amigos.push(input);
+        console.log(amigos.length);
+        mostrarNombres();
+        limpiarCaja();
+    } else {
+        alert('Por favor, inserte un nombre.');
+    }
+    return;
+}
+
+function limpiarCaja(){
+    document.getElementById('amigo').value = '';
+}
+
+function mostrarNombres() {
+    let amigo = document.getElementById('listaAmigos');
+    let tamañoDeLista = amigos.length;
+    console.log(tamañoDeLista);
+    amigo.innerHTML = '';
+    for (let i = 0; i < tamañoDeLista; i++) {
+        amigo.innerHTML +='<li>'+amigos[i]+'</li>';
+    }
+    return;
+}
+
+function sortearAmigo() {   
+}
